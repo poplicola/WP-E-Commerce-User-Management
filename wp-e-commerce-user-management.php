ae64475eadfd9318cee2445b99ab8dbe68100b22
Poplicola <hi@poplicola.com>
2010-08-30 14:19:48 -0500

    Proper - Unregistered Users Update

--- wp-e-commerce-user-management.php
+++ wp-e-commerce-user-management.php
@@ -36,19 +36,32 @@ function wpecom_user_mgmt() {
 		wp_die( __('You do not have sufficient permissions to access this page.') );
 	}
 	global $wpdb;
-	$userinfo = $_GET["userinfo"];
 	
-	echo "<p style='height:20px;'></p>";
+	echo "<p style='padding:20px 0 10px;'>";
 	
 	echo "<a href='admin.php?page=wpecomgmt&thepage=registered' style='float:left;'>";
 	echo "Registered User Data</a>";
-	
 	echo "<a href='admin.php?page=wpecomgmt&thepage=unregistered' style='float:left;margin:0 0 0 20px;'>";
 	echo "Unregistered User Data</a>";
+	
+	echo "</p>";
 	
 	echo"<hr style='width:100%;clear:both;' />";
 	
 	$thepage = $_GET["thepage"];
+	$userinfo = $_GET["userinfo"];
+	
+	$runform = $_POST["runform"];
+	$formids=$_POST["formids"];
+	$log_id=$_POST["userinfo"];
+	$formids = $wpdb->get_results( "SELECT id,name,type FROM wp_wpsc_checkout_forms" );
+	if ($runform==1) {
+		foreach ($formids as $theids) {
+			$id=$theids->id;
+			$updateddata=$_POST[$id];
+			$wpdb->query("UPDATE wp_wpsc_submited_form_data SET value='".$updateddata."' WHERE form_id=".$id." AND log_id=".$log_id);
+		}
+	}
 	
 	if ($thepage=="registered") {
 		/* If $userinfo hasn't filled with requested information, list all registered users */
@@ -65,14 +78,13 @@ function wpecom_user_mgmt() {
 			while ($counter<=$usercount) {
 				$user_ID=$counter; 
 				$meta_data = get_usermeta($user_ID, 'wpshpcrt_usr_profile');
-				echo "<p style='padding:0 0 10px;'><a href='admin.php?page=wpecomgmt&userinfo=" . $user_ID . "'>";
+				echo "<p style='padding:0 0 10px;'><a href='admin.php?page=wpecomgmt&thepage=registered&userinfo=" . $user_ID . "'>";
 				echo $meta_data[2] . " " . $meta_data[3];
 				echo "</a></p>";
 				$counter++;
 			}
 		} else {
 			/* This displays individual user data */
-			global $wpdb;
 			$user_ID=$userinfo;
 			$any_bad_inputs = false;
 			$changes_saved = false;
@@ -255,11 +267,68 @@ function wpecom_user_mgmt() {
 			    </tr>
 			</table>
 			</form>
-			<a href="admin.php?page=wpecomgmt">Back</a>
 			</div><?php
 		}
 	} elseif ($thepage=="unregistered") {
-		echo "Unregistered";
+		if ($userinfo<1) {
+
+			/* Checks how many unregistered users exist */
+			$allUIDs=$wpdb->get_results( "SELECT log_id FROM wp_wpsc_submited_form_data" );
+			$allUIDs=max($allUIDs);
+			$usercount=$allUIDs->log_id;
+			$counter=1;
+			/* End Check */
+
+			while ($counter<=$usercount) {
+				$user_ID=$counter; 
+				$firstname = $wpdb->get_results( "SELECT value FROM wp_wpsc_submited_form_data WHERE log_id=" . $user_ID . " AND form_id=2" );
+				$lastname = $wpdb->get_results( "SELECT value FROM wp_wpsc_submited_form_data WHERE log_id=" . $user_ID . " AND form_id=3" );
+				echo "<p style='padding:0 0 10px;'><a href='admin.php?page=wpecomgmt&thepage=unregistered&userinfo=" . $user_ID . "'>";
+				$firstname = $firstname[0]->value;
+				$lastname = $lastname[0]->value;
+				echo $firstname . " " . $lastname;
+				echo "</a></p>";
+				$counter++;
+			}
+		} else {
+			$user_ID = $userinfo;
+			$formids = $wpdb->get_results( "SELECT id,name,type FROM wp_wpsc_checkout_forms" );
+			echo "<div style='width:350px;'><form method='post' action=''>";
+				foreach ($formids as $theids) {
+					$id=$theids->id;
+					$name=$theids->name;
+					$type=$theids->type;
+					$formdata = $wpdb->get_results( "SELECT value FROM wp_wpsc_submited_form_data WHERE log_id=" . $userinfo . " AND form_id=" . $id );
+					$formdata = $formdata[0]->value;
+					switch ($type) {
+						case "heading";
+						echo "<span style='float:left;clear:both;margin:10px 0 10px;'>" . $name . "</span>";
+						break;
+					}
+					switch ($name) {
+						case "First Name";
+						case "Last Name";
+						case "City";
+						case "Zip Code";
+						case "Email";
+						echo "<span style='float:left;clear:both;margin:0 0 10px;'>" . $name . "</span>";
+						echo "<span style='float:right;'><input type='text' name='" . $id . "' value='" . $formdata . "' onblur=\"if (this.value == '') {this.value = '" . $formdata . "'; this.style.color = '#000000';}\" onfocus=\"if (this.value == '" . $formdata . "') {this.value = ''; this.style.color = '#000000'; }\"></input></span>";
+					break;
+						
+					default:
+						if ($type!="heading" && $name!="Country" && $name!="State") {
+							echo "<span style='float:left;clear:both;'>" . $name . "</span>";
+							echo "<span style='float:right;'><input type='text' name='" . $id . "' value='" . $formdata . "' onblur=\"if (this.value == '') {this.value = '" . $formdata . "'; this.style.color = '#000000';}\" onfocus=\"if (this.value == '" . $formdata . "') {this.value = ''; this.style.color = '#000000'; }\"></input></span>";
+						}
+					break;
+					}
+				}
+			echo "<input type='hidden' value='1' name='runform' />";
+			echo "<input type='hidden' value='" . $formids . "' name='formids' />";
+			echo "<input type='hidden' value='" . $userinfo . "' name='userinfo' />";
+			echo "<input type='submit' value='Submit' name='submit' style='clear:both;float:left;margin:15px 0 0;' />";
+			echo "</form></div>";
+		}
 	}
 }
 
